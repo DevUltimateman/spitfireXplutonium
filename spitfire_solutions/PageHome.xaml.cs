584ecd5d5bc45de55ea99aa0b7cf5506490690a5
DevUltimateman <kingultimateman@gmail.com>
2023-07-17 09:51:36 +0300

    back at work

--- spitfire_solutions/PageHome.xaml.cs
+++ spitfire_solutions/PageHome.xaml.cs
@@ -20,29 +20,147 @@ namespace spitfire_solutions
     /// </summary>
     public partial class PageHome : Page
     {
+        
         public PageHome()
         {
             InitializeComponent();
-        }
+            //initializeAllWindows();,
+            //page Settings on boot up / hidden
 
+            //all views updated & hidden
+            hideAllAtStart();
+        }
+        
+        
+        private PageSettingsGame pSet = new PageSettingsGame();
+        private PageModsGame pMod = new PageModsGame();
+        private PageServersGame pSer = new PageServersGame();
+       
+        
+        private void hideAllAtStart()
+        {
+            pSet.Visibility = Visibility.Hidden;
+            pMod.Visibility = Visibility.Hidden;
+            pSer.Visibility = Visibility.Hidden;
+        }
+        
         private void txtHome_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            framePageView.Content = pageHomeView;
+            framePageView.Visibility = Visibility.Hidden;
         }
 
         private void txtMods_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            if (framePageView.Content != pMod)
+            {
+                Console.WriteLine("Yeah nothing was found, creating the view");
+                framePageView.Content = pMod;
+                if (pMod.Visibility == Visibility.Hidden)
+                {
+                    pMod.Visibility = Visibility.Visible;
+                }
+            }
+            else
+            {
+                if( framePageView.Visibility != Visibility.Visible )
+                {
+                    framePageView.Visibility = Visibility.Visible;
+                }
+                Console.WriteLine("Page " + framePageView.Content.ToString() + " already exists, visibility set TRUE, skipping the callstack");
+            }
         }
-
         private void txtSettings_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            framePageView.Content = new PageSettingsGame();
+            if (framePageView.Content != pSet)
+            {
+                Console.WriteLine("Yeah nothing was found, creating the view");
+                framePageView.Content = pSet;
+                if (pSet.Visibility == Visibility.Hidden)
+                {
+                    pSet.Visibility = Visibility.Visible;
+                }
+            }
+            else
+            {
+                if (framePageView.Visibility != Visibility.Visible)
+                {
+                    framePageView.Visibility = Visibility.Visible;
+                }
+                Console.WriteLine("Page " + framePageView.Content.ToString() + " already exists, visibility set TRUE, skipping the callstack");
+            }
+
         }
 
         private void txtServers_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            if (framePageView.Content != pSer)
+            {
+                Console.WriteLine("Yeah nothing was found, creating the view");
+                framePageView.Content = pSer;
+                if (pSer.Visibility == Visibility.Hidden)
+                {
+                    pSer.Visibility = Visibility.Visible;
+                }
+            }
+            else
+            {
+                if (framePageView.Visibility != Visibility.Visible)
+                {
+                    framePageView.Visibility = Visibility.Visible;
+                }
+                Console.WriteLine("Page " + framePageView.Content.ToString() + " already exists, visibility set TRUE, skipping the callstack");
+            }
+        }
 
+        private void passTheVisibilityCheck( string checking )
+        {
+            
+        }
+        private void keepSettingsAlive()
+        {   
+            //settings
+            pSet.Visibility = Visibility.Visible;
+            //mods
+            pMod.Visibility = Visibility.Hidden;
+            //servers
+            pSer.Visibility = Visibility.Hidden;
+            
+        }
+
+        private void keepHomeAlive()
+        {
+            //settings
+            pSet.Visibility = Visibility.Visible;
+            //mods
+            pMod.Visibility = Visibility.Hidden;
+            //servers
+            pSer.Visibility = Visibility.Hidden;
+            //home
+            this.Visibility = Visibility.Visible;
+        }
+
+        private void keepModsAlive()
+        {
+            //settings
+            pSet.Visibility = Visibility.Hidden;
+            //mods
+            pMod.Visibility = Visibility.Visible;
+            //servers
+            pSer.Visibility = Visibility.Hidden;
+            //home
+            //this.Visibility = Visibility.Visible;
+        }
+
+        private void keepServersAlive()
+        {
+            //settings
+            pSet.Visibility = Visibility.Hidden;
+            //mods
+            pMod.Visibility = Visibility.Hidden;
+            //servers
+            pSer.Visibility = Visibility.Visible;
+            //home
+            //this.Visibility = Visibility.Visible;
         }
     }
 }
