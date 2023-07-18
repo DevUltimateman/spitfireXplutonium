5af61ac841bf9b46e4739f899495f8e0e7df7be1
DevUltimateman <kingultimateman@gmail.com>
2023-07-18 19:49:11 +0300

    lovelyjubbly

--- spitfire_solutions/MainWindow.xaml.cs
+++ spitfire_solutions/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+using System.Runtime.InteropServices;
+using System.Runtime;
+using System.Windows.Interop;
+
 namespace spitfire_solutions
 {
     /// <summary>
@@ -26,6 +31,43 @@ namespace spitfire_solutions
 
             //one time initialization
             //frameMainLocked.Content = new PageHome();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+        //to hold down the window + to move
+        private void stckControlPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+            //DragMove();
+        }
+
+        private void stckControlPanel_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+            //Environment.Exit(1);
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState != WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else { this.WindowState = WindowState.Normal; }
         }
     }
 }
